05dd127067c3d275a09fa876866ae89306b314ea
aarondoesnotcode <aaronparwany.p@gmail.com>
2026-05-05 01:30:43 +0100

    adding skill section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
+import Skills from "./components/Skills.jsx";
 
 function App() {
   return (
     <main>
       <Hero />
       <About bio="I am a QMUL student" />
+      <Skills />
     </main>
   );
 }
